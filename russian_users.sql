80e487301aed7eeebd875bfe28df73370cf2ac7f
Valeriy Kozyar <51660599+KozyarValeriy@users.noreply.github.com>
2019-08-06 17:46:53 +0300

    Update russian_users.sql

--- russian_users.sql
+++ russian_users.sql
@@ -1,11 +1,11 @@
 SELECT
-	users.id,
-	users.first_name,
-	users.last_name,
-	users.email,
+    users.id,
+    users.first_name,
+    users.last_name,
+    users.email,
     counties.name AS country
 FROM users, cities, counties
 WHERE users.city_id = cities.id 
-	AND cities.country_id = counties.id 
-	AND counties.name = 'Russia'
+    AND cities.country_id = counties.id 
+    AND counties.name = 'Russia'
 ORDER BY users.id;
